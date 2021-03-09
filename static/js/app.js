703bd355f778cdc01a8e6489d8bde6512b8b13b1
Phillecia Qualls <plqualls@msn.com>
2021-03-08 19:50:07 -0600

    Updated code w/o errors

--- static/js/app.js
+++ static/js/app.js
@@ -1,80 +1,83 @@
-function getPlots(id)
-
-//Read samples.json file
-d3.json("samples.json").then (sampledata =>{
-
-    console.log(sampledata)
-
-    var ids = sampledata.samples[0].otu_ids;
-    console.log(ids)
-
-    var sampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
-    console.log(sampleValues)
-
-    var labels = (sampledata.samples[0].otu_ids.slice(0,10)).reverse();
-    console.log (labels)
-
-    //Retrieve the top ten otu_ids for the plot.
-    var OTU_top = (sampledata.samples[0].otu_ids.slice(0,10)).reverse();
-
-    //Get the id's to the form for the plot.
-    var OTU_id = OTU_top.map(d => "OTU" + d);
-    console.log(`OTU IDS: ${OTU_id}`)
-
-    //Retrieve the plots top ten labels.
-    var labels = sampledata.samples[0].otu_labels.slice(0,10);
-    console.log(`OTU_labels: ${labels}`)
-    var trace = {
-        x: sampleValues,
-        y: OTU_id,
-        text: labels,
-        marker: {
-        color: 'blue'},
-        type: "bar",
-        orientation: "h",
-    };
-    //Creating data variables
-    var data = [trace];
-
-    //Setting and creating plots layout.
-    var layout = {
-        title: "Top 10 OTU",
-        yaxis:{
-            tickmode:"linear",
-        },
-        margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 30,
-        }
-    };
-    //Create bar plot
-Plotly.newPlot("bar", data, layout);
-
-    //Bubble Chart
-    var trace1 = {
-        x: sampledata.samples[0].otu_ids,
-        y: sampledata.samples[0].sample_values,
-        mode: "markers",
-        marker: {
-            size: sampledata.samples[0].sample_values,
-            color: sampledata.samples[0].otu_ids
-        }, 
+function getPlots(id) {
+    //Read samples.json
+        d3.json("samples.json").then (sampledata =>{
+            console.log(sampledata)
+            var ids = sampledata.samples[0].otu_ids;
+            console.log(ids)
+            var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+            console.log(sampleValues)
+            var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+            console.log (labels)
+        // get only top 10 otu ids for the plot OTU and reversing it. 
+            var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+        // get the otu id's to the desired form for the plot
+            var OTU_id = OTU_top.map(d => "OTU " + d);
+            console.log(`OTU IDS: ${OTU_id}`)
+         // get the top 10 labels for the plot
+            var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+            console.log(`OTU_labels: ${labels}`)
+            var trace = {
+                x: sampleValues,
+                y: OTU_id,
+                text: labels,
+                marker: {
+                color: 'blue'},
+                type:"bar",
+                orientation: "h",
+            };
+            // create data variable
+            var data = [trace];
     
-        text : sampledata.samples[0].otu_labels
+            // create layout variable to set plots layout
+            var layout = {
+                title: "Top 10 OTU",
+                yaxis:{
+                    tickmode:"linear",
+                },
+                margin: {
+                    l: 100,
+                    r: 100,
+                    t: 100,
+                    b: 30
+                }
+            };
+    
+            // create the bar plot
+        Plotly.newPlot("bar", data, layout);
+            // The bubble chart
+            var trace1 = {
+                x: sampledata.samples[0].otu_ids,
+                y: sampledata.samples[0].sample_values,
+                mode: "markers",
+                marker: {
+                    size: sampledata.samples[0].sample_values,
+                    color: sampledata.samples[0].otu_ids
+                },
+                text:  sampledata.samples[0].otu_labels
+    
+            };
+    
+            // set the layout for the bubble plot
+            var layout_2 = {
+                xaxis:{title: "OTU ID"},
+                height: 600,
+                width: 1000
+            };
+    
+            // creating data variable 
+            var data1 = [trace1];
+    
+        // create the bubble plot
+        Plotly.newPlot("bubble", data1, layout_2); 
         
-    };
-    //Setting layout for bubble plot
-    var layout_2 = {
-        xaxis:{title: "OTU ID"},
-        height: 600,
-        width: 1000
-    };
-    
-    
+        });
     }
 
+//Get necessary data
 
-})
+
+
+
+    
+  
 
